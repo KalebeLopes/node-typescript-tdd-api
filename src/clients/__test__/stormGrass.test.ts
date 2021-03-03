2a630f0fba0e58e45d066fbc5b7ccbe15db3e8f9
KalebeLopes <calbkalebe@gmail.com>
2021-03-03 15:39:15 -0300

    test: communication with external api

---
+++ src/clients/__test__/stormGrass.test.ts
@@ -0,0 +1,22 @@
+import { StormGlass } from '../stormGlass'
+import axios from 'axios'
+import stormGlassWeather3HoursFixture from '../../../test/fixtures/stormGlass_weather_3_hours.json'
+import stormGlassNormalized3HoursFixture from '../../../test/fixtures/stormGlass_normalized_response_3_hours.json'
+
+
+
+jest.mock('axios')
+
+describe('StormGlass client', () => {
+  it('should return te normalized forecast frm the StormGlass service', async() => {
+    const lat = -33
+    const lng = 151
+
+    axios.get = jest.fn().mockResolvedValue(stormGlassWeather3HoursFixture) // mock do jest
+
+    const stormGlass = new StormGlass(axios);
+    const response = await stormGlass.fetchPoints(lat, lng)
+    
+    expect(response).toEqual(stormGlassNormalized3HoursFixture)
+  })
+})
